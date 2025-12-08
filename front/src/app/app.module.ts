c6a59d0c41ead917964bb552170c8c427e57cee4
lucas <lucascassian.lc@gmail.com>
2025-12-08 08:00:08 -0300

    teste mascara

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, NgxMaskDirective, NgxMaskPipe ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideNgxMask()],
   bootstrap: [AppComponent],
 
 })
